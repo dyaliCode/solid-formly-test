9003de9af8d7c6e2a8ea2c5d6a529737e000ad44
kamalkech <maroc.develop@gmail.com>
2022-06-30 16:47:50 +0100

    .

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,4 +1,5 @@
 import { createSignal, createRoot } from "solid-js";
+import { createStore } from "solid-js/store";
 
 type IFormStore = {
   name: string;
@@ -7,14 +8,16 @@ type IFormStore = {
 
 // Forms
 function _formsStore() {
-  const [forms, setForms] = createSignal<IFormStore[]>([]);
+  // const [forms, setForms] = createSignal<IFormStore[]>([]);
+  const [forms, setForms] = createStore([]);
   return { forms, setForms };
 }
-export const formsStore: any = createRoot(_formsStore);
+export const formsStore: any = _formsStore; //createRoot(_formsStore);
 
 // Current form
 function _curentFormStore() {
-  const [currentForm, setCurrentForm] = createSignal<any>([]);
+  // const [currentForm, setCurrentForm] = createSignal<any>([]);
+  const [currentForm, setCurrentForm] = createStore([]);
   return { currentForm, setCurrentForm };
 }
 export const curentFormStore: any = createRoot(_curentFormStore);
